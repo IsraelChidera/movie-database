0d82473516fb01b28feaaf80faf432b04f99e9ec
israelchidera <israelchidera54@gmail.com>
2021-05-13 23:55:04 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import './App.css';
+import Search from './components/Search';
+import Results from './components/Results';
+import Popup from './components/Popup';
+import React, {useState} from 'react';
+import axios from 'axios';
+
+function App() {
+  const [state, setState] = useState({ 
+    s: "",
+    results: [],
+    selected: {}
+  })
+  const apiurl = "https://www.omdbapi.com/?i=tt3896198&apikey=43c5b1de";
+
+  const search = (e) => {
+    if(e.key === "Enter") {
+      axios(apiurl + "&s=" + state.s).then(({data}) => {
+        let results = data.Search;
+        console.log(results)
+        setState(prevState => {
+          return {...prevState, results: results}
+        })
+      })
+    }
+  }
+
+  const handleInput = (e) => {
+    let s = e.target.value;
+
+    setState(prevState => {
+      return {...prevState, s: s}
+    });
+  }
+
+  const openPopup = (id) => {
+    axios(apiurl + "&i=" + id).then(({data}) => {
+      let result = data;
+
+      console.log(result);
+
+      setState(prevState => {
+        return {...prevState, selected: result}
+      })
+    });
+  }
+
+  const closePopup = () => {
+    setState(prevState => {
+      return {...prevState, selected: {}}
+    })
+  }
+
+  return (
+    <div className="App">
+      <header>
+        <h1>IMDB Movie Database</h1>
+      </header>
+      <main>
+        <Search handleInput={handleInput} search={search}/>
+        <Results results={state.results} openPopup={openPopup} />
+        
+        
+        {(typeof state.selected.Title !== "undefined") ? <Popup selected={state.selected} 
+        closePopup={closePopup} /> : false}
+      </main>
+    </div>
+  );
+}
+ 
+export default App;
